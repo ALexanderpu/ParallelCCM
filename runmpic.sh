4b73214b67726b5e2f7106e644891f2c34198dcd
Alexander <pubo.alexander@gmail.com>
2019-04-01 15:59:47 -0600

    test

--- runmpic.sh
+++ runmpic.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "mpi will not use cuda lib: please remove rank_matrix_gpu in global.h"
+
+echo "make sure mpich2 has been installed on your system"
 
 
 echo "compiling the program for MPI distributed version of ccm"
 
-mpicxx -fopenmp -std=c++11 -o ./MPIVersion/MPIInterface ./CCM/MPIInterface.cpp
+mpicxx -fopenmp -std=c++11 -o ./MPIVersion/MPIInterface ./CCM/MPIInterface.cpp ./CCM/include/cpu_sort.cpp
+
 
 echo "running mpi distributed version of ccm"
 mpirun -np 3 --hosts 10.80.64.110,10.80.64.35,10.80.64.53 ./MPIVersion/MPIInterface
