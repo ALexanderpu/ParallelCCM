9afb8193dfa09391de69f099a248899dfded89b9
Alexander <pubo.alexander@gmail.com>
2019-04-03 15:20:14 -0600

    Complete for three versions

--- SparkCCM.py
+++ SparkCCM.py
@@ -9,7 +9,7 @@ import json
 import numpy as np
 import os
 
-# for single L
+# for single L; which will be not used 
 # read parameter combinations config and fill into the objects
 class Sample:
     def __init__(self, _observations, _targets, _e, _tau, _l, _samples, _multil, _genoutput):
@@ -41,107 +41,84 @@ def ccm(LArr, EArr, TauArr, num_samples, time_series, x, y, scriptPath, generate
     return result
 
 
-# for multi Ls
+# for multi Ls in one task
 class SampleMultiL:
-    def __init__(self, _observations, _targets, _e, _tau, _samples, _lstart, _lend, _linterval, _multil, _genoutput):
+    def __init__(self, _observations, _targets, _e, _tau, _samples, _lstart, _lend, _linterval, _multil, _genoutput, _outputpath, _gpu):
         self.observations = _observations
         self.targets = _targets
         self.e = _e
         self.tau = _tau
         self.samples = _samples
-        self.multil = _multil
-        self.genoutput = _genoutput
         self.lstart = _lstart
         self.lend = _lend
         self.linterval = _linterval
+        self.multil = _multil
+        self.genoutput = _genoutput
+        self.outputpath = _outputpath
+        self.gpu = _gpu
 
-
-def ccmMultiL(LStart, LEnd, LInterval, EArr, TauArr, num_samples, time_series, x, y, scriptPath, generateOutput):
-    observations, targets = time_series[x].tolist(), time_series[y].tolist()
-    paras = []
-    for e in EArr:
-        for tau in TauArr:
-            s = SampleMultiL(observations, targets, e, tau, num_samples, LStart, LEnd, LInterval, 1, generateOutput)
-            para = json.dumps(vars(s))
-            #print para
-            paras.append(para)
-    # start the spark context 
-    spark = SparkSession.builder.appName("PySparkCCMMultiL").getOrCreate()
-    paraRdd = spark.sparkContext.parallelize(paras)
-    piped = paraRdd.pipe(scriptPath)
-    result = piped.collect()
-    for ele in result:
-        print(ele)
-    
-    spark.stop()
-    
-    return result
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("using default cfg file path")
-        cfgfile = "/home/bo/cloud/CCM-Parralization/ccm.cfg"
-    else:
-        # read input cfg file: the first argument is the file path
-        cfgfile = sys.argv[1]
+        print("please input the local path of ccm.cfg")
+        sys.exit()
+
+    # read input cfg file: the first argument is the file path
+    cfgfile = sys.argv[1]
         
     config = ConfigParser.RawConfigParser()
     config.read(cfgfile)
     try:
-        MultiLsVersion = config.getboolean('options', 'MultiLsVersion')
-        GenerateOutputCSV = config.getint('options', 'GenerateOutputCSV')
-
         input_path = config.get('paths', 'input')
         output_path = config.get('paths', 'output')
         script_path = config.get('paths', 'sparkccmlib')
 
         E = config.get('parameters', 'E')
-        tau = config.get('parameters', 'tau')
+        Tau = config.get('parameters', 'tau')
         EArr = map(int, E.split(","))
-        tauArr = map(int, tau.split(","))
+        TauArr = map(int, Tau.split(","))
 
         num_samples = config.getint('parameters', 'num_samples')
         
         LStart = config.getint('parameters', 'LStart')
         LEnd = config.getint('parameters', 'LEnd')
         LInterval = config.getint('parameters', 'LInterval')
-        LArr = range(LStart, LEnd+1, LInterval)
 
-        x = config.get('inputs', 'x')
-        y = config.get('inputs', 'y')
-
+        xname = config.get('inputs', 'x')
+        yname = config.get('inputs', 'y')
 
         time_series = pd.read_csv(input_path)
+        observations, targets = time_series[xname].tolist(), time_series[yname].tolist()
 
-        # start to run ccm algorithm
-        if MultiLsVersion:
-            result = ccmMultiL(LStart, LEnd, LInterval, EArr, tauArr, num_samples, time_series, x, y, script_path, GenerateOutputCSV)
-            # output path in the result
-            with open("outputcsvpath.out", "w") as f:
-                for record in result:
-                    f.write(record)
-        else:
-            result = ccm(LArr, EArr, tauArr, num_samples, time_series, x, y, script_path, GenerateOutputCSV)
-            # save csv file to output folder
-            # save result into the local file: define the format
-            labels = ['E', 'tau', 'L', 'rho']
-            taus = []
-            es = []
-            ls = []
-            rhos = []
-            for r in result:
-                record = json.loads(r)
-                rho = record['result']  # a list
-                num_samples = len(rho)
-                tau = record['tau']
-                e = record['e']
-                l = record['l']
-                rhos.extend(rho)
-                taus.extend([tau] * num_samples)
-                es.extend([e] * num_samples)
-                ls.extend([l] * num_samples)
-            result_df = pd.DataFrame(np.column_stack([es, taus, ls, rhos]), columns=labels)
-            output_file = output_path + "/sparkccm.csv"
-            result_df.to_csv(output_file)
+        GenerateOutputCSV = config.getint('options', 'GenerateOutputCSV')
+        GPUAcceleration = config.getint('options', 'GPUAcceleration')
+        print("GPUAcceleration: " + str(GPUAcceleration))
+        # generate para rdd to separate the tasks to different workers
+        paras = []
+        for e in EArr:
+            for tau in TauArr:
+                s = SampleMultiL(observations, targets, e, tau, num_samples, LStart, LEnd, LInterval, 1, GenerateOutputCSV, output_path, GPUAcceleration)
+                para = json.dumps(vars(s))
+                #print para
+                paras.append(para)
+        # start the spark context 
+        
+        print("size: " + str(len(paras)))
+        
+        
+        spark = SparkSession.builder.appName("PySparkCCMMultiL").getOrCreate()
+        paraRdd = spark.sparkContext.parallelize(paras)
+        piped = paraRdd.pipe(script_path)
+        result = piped.collect()
+
+        for ele in result:
+            print(ele)
+
+        spark.stop()
+
+        # output path in the result
+        # with open("outputcsvpath.out", "w") as f:
+        #    for record in result:
+        #        f.write(record)
     except:
         print("parsing config file error")
