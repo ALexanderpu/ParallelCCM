96d635623697952d2016263e3308f6d4362ea6d4
Alexander <pubo.alexander@gmail.com>
2019-04-02 20:09:38 -0600

    Complete MPI Cluster Version

--- CCM/MPIInterface.cpp
+++ CCM/MPIInterface.cpp
@@ -6,11 +6,15 @@
 using namespace std;
 
 int main(int argc, char **argv){
-    string file = "/home/bo/cloud/CCM-Parralization/ccm.cfg";
+    
+    if(argc <= 1){
+        std::cout << "please pass the config file as the first argument!" << std::endl;
+        return 1;
+    }
     ConfigReader cr;
     int num_samples;
     std::vector<int> EArr, tauArr, LArr;
-    string input, output;
+    std::string input, output;
     vector<float > observations, targets;
     int num_vectors;
 
@@ -29,11 +33,93 @@ int main(int argc, char **argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     MPI::Get_processor_name(processor_name, namelen);
-    cout << "Process ID " << my_id << " is on " << processor_name << endl;
+    std::cout << "Worker ID " << my_id << " is on " << processor_name << endl;
+    bool flag = false;
+    // read settings from local file
+    string file(argv[1]);
+    try{
+        // parse config file: /home/bo/cloud/CCM-Parralization/ccm.cfg
+        cr.read_file(file);
 
+        // read parameters and scatter to other processor
+        string E = cr.get_string("parameters", "E");
+        string tau = cr.get_string("parameters", "tau");
+
+        // parse to int vector for parameter: tau, E
+        std::stringstream Ess(E), tauss(tau);
+        int i;
+        while(Ess >> i){
+            EArr.push_back(i);
+            if(Ess.peek() == ',')
+                Ess.ignore();
+        }
+        while(tauss >> i){
+            tauArr.push_back(i);
+            if(tauss.peek() == ',')
+                tauss.ignore();
+        }
+
+        num_samples = stoi(cr.get_string("parameters", "num_samples"));
+
+        // parse to int for parameter: L
+        int LStart = stoi(cr.get_string("parameters", "LStart"));
+        int LEnd = stoi(cr.get_string("parameters", "LEnd"));
+        int LInterval = stoi(cr.get_string("parameters", "LInterval"));
+        size_t Lsize = (LEnd-LStart)/LInterval + 1;
+        
+        LArr.assign(Lsize, 0);
+        IncGenerator g (LStart-LInterval, LInterval);
+        std::generate(LArr.begin(), LArr.end(), g);
+
+        output = cr.get_string("paths", "output");
+
+        if(my_id == 0){
+            // read csv file for observations and targets to broadcast to other processor
+            input = cr.get_string("paths", "input");
+            string xname = cr.get_string("inputs", "x");
+            string yname = cr.get_string("inputs", "y");
+            std::tie(observations, targets) = parse_csv(input, xname, yname);
+            num_vectors = observations.size();
+            if(observations.size() != targets.size()){
+                std::cout << "input sequence length not match" << std::endl;
+                flag = true;
+            }
+            // broadcast tasks
+            num_tasks = tauArr.size()*EArr.size()*LArr.size();
+            elements_per_proc = num_tasks / num_procs; // should be dividable 
+
+            for(auto e: EArr){
+                for(int ele = 0; ele < tauArr.size()*LArr.size(); ele++){
+                    combinationEArr.push_back(ele);
+                }
+                
+                for(auto tau: tauArr){
+                    for(int ele = 0; ele < LArr.size(); ele++){
+                        combinationtauArr.push_back(ele);
+                    }   
+                    for(auto l: LArr){
+                        combinationLArr.push_back(l);
+                    }
+                }
+            }
+        }
+
+        MPI_Bcast(&flag, 1, MPI_CXX_BOOL, 0, MPI_COMM_WORLD);
+
+    }catch(int e){
+        std::cout << "loading data and parameter error" << std::endl;
+        return 1;
+    }
+
+    if(flag){ // input sequence is not match
+        return 1;
+    }
+
+
+    /*
     if(my_id == 0){
         // read settings from local file
-        
+        string file(argv[1]);
         try{
             // parse config file: /home/bo/cloud/CCM-Parralization/ccm.cfg
             cr.read_file(file);
@@ -69,8 +155,9 @@ int main(int argc, char **argv){
             std::generate(LArr.begin(), LArr.end(), g);
 
             // read csv file for observations and targets to broadcast to other processor
-            string input = cr.get_string("paths", "input");
-            string output = cr.get_string("paths", "output");
+            input = cr.get_string("paths", "input");
+            output = cr.get_string("paths", "output");
+            cout << "output directory: " << output << endl;
             string xname = cr.get_string("inputs", "x");
             string yname = cr.get_string("inputs", "y");
 
@@ -81,16 +168,6 @@ int main(int argc, char **argv){
             return 1;
         }
 
-        if(observations.size() != targets.size()){
-            cout << "input sequence length not match" << endl;
-            return 1;
-        }
-        /*
-        num_vectors = 5;
-        
-        observations.assign(num_vectors, 3);
-        targets.assign(num_vectors, 5);
-        */
        // how to scatter the parameter settings, E tau, L to assign task to each processor?
        num_tasks = tauArr.size()*EArr.size()*LArr.size();
        elements_per_proc = num_tasks / num_procs; // should be dividable 
@@ -110,6 +187,7 @@ int main(int argc, char **argv){
            }
        }
     }
+    */
 
     MPI_Bcast(&num_vectors, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&num_samples, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -146,10 +224,11 @@ int main(int argc, char **argv){
     for(int i = start_index; i <= end_index; i++){
         CCMParallel cp;
         vector<float> result = cp.ccm(observations, targets, combinationEArr[i], combinationtauArr[i], combinationLArr[i], num_samples);
-        cout << "task " << i << " with id:  " << my_id << " has the first result: " << result[0] << " size is: " << result.size() << endl;
+        std::cout << "task " << i << " with id:  " << my_id << " has the first result: " << result[0] << " size is: " << result.size() << endl;
         // better to save result into csv here
-        //std::string csvfile = output +  "/e_" + to_string(combinationEArr[i]) + "_tau_" + to_string(combinationtauArr[i]) + "_l_" + to_string(combinationLArr[i]) + "_MPIversion.csv";
-        //dump_csv(csvfile, result, (size_t)combinationEArr[i], (size_t)combinationtauArr[i], (size_t)combinationLArr[i]);
+        std::string csvfile = output +  "/e_" + to_string(combinationEArr[i]) + "_tau_" + to_string(combinationtauArr[i]) + "_l_" + to_string(combinationLArr[i]) + "_MPIversion.csv";
+        std::cout << "task " << i << " with id:  " << my_id << "write file: " << csvfile <<endl;
+        dump_csv(csvfile, result, (size_t)combinationEArr[i], (size_t)combinationtauArr[i], (size_t)combinationLArr[i]);
         //float* a = &result[0];
         //MPI_Gather(a, num_samples, MPI_FLOAT, finalResult[i], num_samples, MPI_FLOAT, 0, MPI_COMM_WORLD);
     }
